2d571011a3463a17f54bd55ceab363b607b0f274
peres24 <davidperesfilho24@gmail.com>
2026-03-31 14:57:27 -0300

    continuacao

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 public class Main{
 public static void main(String[]args){
-    String frase = "Quero ser programador. Aprendi Java no Devisate. Marilia", ajuste;
-    ajuste = frase.toUpperCase();
-    System.out.printf(ajuste);
-    // String secreta = "QHSAVOIPOPRERRDTJIERSSMVGIEROEEMNAAARDAAANL";
+    String frase = "Quero ser programador. Aprendi Java no Devisate. Marilia";
+    String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    String cifrador[];
+    for(int i = 0; i < frase.length(); i++){
+        char letra = frase.toUpperCase().charAt(i);
+        if(letra != ' ' && letra != '.'){
+            cifrador = cifrador[letra];
+        }
+    }
+
+    
 }
 }
