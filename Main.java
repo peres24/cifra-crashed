3aa7eb4a151c7a6fcfd71d72a704dd7378af95c1
peres24 <davidperesfilho24@gmail.com>
2026-03-31 08:45:45 -0300

    primeiras linhas de codigo cifrador

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+public static void main(String[]args){
+    String frase = "Quero ser programador. Aprendi Java no Devisate. Marilia", ajuste;
+    ajuste = frase.toUpperCase();
+    System.out.printf(ajuste);
+    // String secreta = "QHSAVOIPOPRERRDTJIERSSMVGIEROEEMNAAARDAAANL";
+}
+}
