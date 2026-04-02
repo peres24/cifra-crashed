8551f5ac18cdec4a4ed51f574f7512f50864d6a8
peres24 <davidperesfilho24@gmail.com>
2026-04-02 09:38:24 -0300

    codigo esta comecando a pegar forma

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
 public class Main{
 public static void main(String[]args){
-    String frase = "Quero ser programador. Aprendi Java no Devisate. Marilia";
+    Scanner ler = new Scanner(System.in);
+    String frase;
+    System.out.println("Digite sua frase:");
+    frase = ler.nextLine();
+    char[] cifrador = new char[frase.length()];
     String[] letras = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    String cifrador[];
+    
     for(int i = 0; i < frase.length(); i++){
         char letra = frase.toUpperCase().charAt(i);
         if(letra != ' ' && letra != '.'){
-            cifrador = cifrador[letra];
+            cifrador[i] = letra;
         }
     }
 
-    
+    System.out.print(cifrador);
+    // for(i; i < frase.length(); i++){
+    //     i = (i + 5)%{frase.length()};
+        
+    // }
 }
 }
