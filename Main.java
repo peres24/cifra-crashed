3036fde6a800cc68231f347e74a0cc8f6a9e0c7c
unknown <davidperesfilho24@gmail.com>
2026-04-08 08:53:04 -0300

    dando certo

--- Main.java
+++ Main.java
@@ -6,18 +6,30 @@ public static void main(String[]args){
     System.out.println("Digite sua frase:");
     frase = ler.nextLine();
     char[] cifrador = new char[frase.length()];
+    char[] pontos = {'.', ';', ':', '>', '<', '"', '!', '@' '#', '$', '%', '¨' , '&', '*', '(', ')', '´', '`', '-', '_', '=', '+', '§', '[', ']', '^', '~', 'º', '{', '}', '?', '/', '°'}
     
-    for(int i = 0; i < frase.length(); i++){
+     for(int i = 0; i < frase.length(); i++){
         char letra = frase.toUpperCase().charAt(i);
         if(letra != ' ' && letra != '.'){
             cifrador[i] = letra;
         }
-    }
-
-    System.out.print(cifrador);
-    // for(i; i < cifrador.length(); i++){
-    //     i = (i + 5)%frase.length();
-    //     System.out.print(i);
+        else{
+            continue;
+        }
+    //     for(int i = 0; i < frase.length(); i++){
+    //     char letra = frase.toUpperCase().charAt(i);
+    //     for(i = 0; i < frase.length(); i++){
+    //         ponto = pontos[i];
+    //         if(letra == ponto){
+    //         continue;
+    //     }
+    //     else{
+    //         cifrador[i] = letra;
+    //     }
+    //     }
+    //     System.out.print(cifrador[i]);
     // }
+        System.out.println(cifrador[i]);
+    }
 }
 }
