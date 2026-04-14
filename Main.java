e43736e54a1518c1f55e10d29c75ec59b0619ad5
unknown <davidperesfilho24@gmail.com>
2026-04-14 09:37:14 -0300

    estou quase conseguindo

--- Main.java
+++ Main.java
@@ -1,27 +1,21 @@
 import java.util.Scanner;
 public class Main{
 public static void main(String[]args){
-    Scanner ler = new Scanner(System.in);
+    Scanner ler = new Scanner(System.in, "cp850");
     String frase;
     System.out.println("Digite sua frase:");
     frase = ler.nextLine();
     char[] cifrador = new char[frase.length()];
-    char[] pontos = {'.', ';', ':', '>', '<', '"', '!', '@', '#', '$', '%', '¨' , '&', '*', '(', ')', '´', '`', '-', '_', '=', '+', '§', '[', ']', '^', '~', 'º', '{', '}', '?', '/', '°'};
     
-    //  for(int i = 0; i < frase.length(); i++){
-    //     char letra = frase.toUpperCase().charAt(i);
-    //     if(letra != ' ' && letra != '.'){
-    //         cifrador[i] = letra;
-    //     }
-    //     else{
-    //         continue;
-    //     }
         for(int i = 0; i < frase.length(); i++){
         char letra = frase.toUpperCase().charAt(i);
-        char arrumado = String.valueOf(letra).replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ç", "C").replace("Ó", "O").replace("Ú", "U").charAt(0);
-        // for(int j = 0; j < pontos.length; j++){
-        //     if(letra != pontos[j]){
-                cifrador[i] = arrumado;
+        char arrumado = String.valueOf(letra).replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ç", "C").replace("Ó", "O").replace("Ú", "U").replace(".", " ").replace(",", " ").replace(";", " ").replace(",", " ").replace(":", " ").replace(",", " ").replace(">", " ").replace(",", " ").replace("<", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("!", " ").replace(",", " ").replace("@", " ").replace(",", " ").replace("#", " ").replace(",", " ").replace("$", " ").replace(",", " ").replace("%", " ").replace(",", " ").replace("¨", " ").replace(" ", " ").replace("&", " ").replace(",", " ").replace("*", " ").replace(",", " ").replace("(", " ").replace(",", " ").replace(")", " ").replace(",", " ").replace("´", " ").replace(",", " ").replace("`", " ").replace(",", " ").replace("-", " ").replace(",", " ").replace("_", " ").replace(",", " ").replace("=", " ").replace(",", " ").replace("+", " ").replace(",", " ").replace("§", " ").replace(",", " ").replace("[", " ").replace(",", " ").replace("]", " ").replace(",", " ").replace("^", " ").replace(",", " "). replace("~", " ").replace(",", " ").replace("º", " ").replace(",", " ").replace("{", " ").replace(",", " ").replace("}", " ").replace(",", " ").replace("?", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("°", " ").replace("}", " ").charAt(0);
+        if(arrumado != ' '){
+            cifrador[i] = arrumado;
+        }
+        // for(int j = 0; j < frase.length(); j++){
+        //     if(arrumado != ' '){
+        //         cifrador[i] = arrumado;
         // }
         // else{
         //     continue;
