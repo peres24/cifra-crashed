412133ec5b2833ecfc836b9de77b446dcfc3906b
peres24 <davidperesfilho24@gmail.com>
2026-04-14 20:28:17 -0300

    deu certo vamooooooooooooooooo

--- Main.java
+++ Main.java
@@ -1,27 +1,24 @@
 import java.util.Scanner;
 public class Main{
 public static void main(String[]args){
-    Scanner ler = new Scanner(System.in, "cp850");
+    Scanner ler = new Scanner(System.in, "Cp850");
     String frase;
     System.out.println("Digite sua frase:");
     frase = ler.nextLine();
     char[] cifrador = new char[frase.length()];
     
-        for(int i = 0; i < frase.length(); i++){
-        char letra = frase.toUpperCase().charAt(i);
-        char arrumado = String.valueOf(letra).replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ç", "C").replace("Ó", "O").replace("Ú", "U").replace(".", " ").replace(",", " ").replace(";", " ").replace(",", " ").replace(":", " ").replace(",", " ").replace(">", " ").replace(",", " ").replace("<", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("!", " ").replace(",", " ").replace("@", " ").replace(",", " ").replace("#", " ").replace(",", " ").replace("$", " ").replace(",", " ").replace("%", " ").replace(",", " ").replace("¨", " ").replace(" ", " ").replace("&", " ").replace(",", " ").replace("*", " ").replace(",", " ").replace("(", " ").replace(",", " ").replace(")", " ").replace(",", " ").replace("´", " ").replace(",", " ").replace("`", " ").replace(",", " ").replace("-", " ").replace(",", " ").replace("_", " ").replace(",", " ").replace("=", " ").replace(",", " ").replace("+", " ").replace(",", " ").replace("§", " ").replace(",", " ").replace("[", " ").replace(",", " ").replace("]", " ").replace(",", " ").replace("^", " ").replace(",", " "). replace("~", " ").replace(",", " ").replace("º", " ").replace(",", " ").replace("{", " ").replace(",", " ").replace("}", " ").replace(",", " ").replace("?", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("°", " ").replace("}", " ").charAt(0);
-        if(arrumado != ' '){
+     for(int i = 0; i < frase.length(); i++){
+     char letra = frase.toUpperCase().charAt(i);
+char arrumado = String.valueOf(letra).replace("Á", "A").replace("É", "E").replace("Í", "I").replace("Ç", "C").replace("Ó", "O").replace("Ú", "U").replace(".", " ").replace(",", " ").replace(";", " ").replace(",", " ").replace(":", " ").replace(",", " ").replace(">", " ").replace(",", " ").replace("<", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("!", " ").replace(",", " ").replace("@", " ").replace(",", " ").replace("#", " ").replace(",", " ").replace("$", " ").replace(",", " ").replace("%", " ").replace(",", " ").replace("¨", " ").replace(" ", " ").replace("&", " ").replace(",", " ").replace("*", " ").replace(",", " ").replace("(", " ").replace(",", " ").replace(")", " ").replace(",", " ").replace("´", " ").replace(",", " ").replace("`", " ").replace(",", " ").replace("-", " ").replace(",", " ").replace("_", " ").replace(",", " ").replace("=", " ").replace(",", " ").replace("+", " ").replace(",", " ").replace("§", " ").replace(",", " ").replace("[", " ").replace(",", " ").replace("]", " ").replace(",", " ").replace("^", " ").replace(",", " "). replace("~", " ").replace(",", " ").replace("º", " ").replace(",", " ").replace("{", " ").replace(",", " ").replace("}", " ").replace(",", " ").replace("?", " ").replace(",", " ").replace("/", " ").replace(",", " ").replace("°", " ").replace("}", " ").charAt(0);
+        letra = arrumado;
+        if(letra != ' '){
             cifrador[i] = arrumado;
         }
-        // for(int j = 0; j < frase.length(); j++){
-        //     if(arrumado != ' '){
-        //         cifrador[i] = arrumado;
-        // }
-        // else{
-        //     continue;
-        // }
-        // }
+        else{
+            continue;
         }
-        System.out.print(cifrador);
+
+        System.out.print(cifrador[i]);
     }
+}
 }
